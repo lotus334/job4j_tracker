10838b409030d692941ade6ead8ca56ef9b5a548
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-15 19:57:54 +0700

    6. Date. Отображение даты. [#386410]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    LocalDateTime created;
+
+    public Item() {
+        this.created = LocalDateTime.now();
+    }
 
     public int getId() {
         return id;
@@ -19,4 +26,6 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {return created; }
 }
