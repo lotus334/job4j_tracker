6d75869f41df347be45124ad970ddf17cac53180
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-16 08:49:07 +0700

    7. toString [#386417]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -36,4 +36,13 @@ public class Item {
     }
 
     public LocalDateTime getCreated() {return created; }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
 }
