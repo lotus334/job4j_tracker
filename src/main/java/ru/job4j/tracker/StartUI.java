76d78e9bbe36ac2b0e401543a2357bb2a5da2ae5
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-18 08:20:40 +0700

    2.1. Реализация класса StartUI [#386361] исправлен алгоритм при select == 2. Убран вложенный цикл. Убраны nextLine.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,44 +5,37 @@ import java.util.Scanner;
 public class StartUI {
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
+        int select;
         while (run) {
             this.showMenu();
             System.out.print("Select: ");
-            int select = 7;
-            String choose = scanner.nextLine();
-            while (!choose.matches("[0-6]")) {
-                choose = scanner.nextLine();
-            }
-            select = Integer.parseInt(choose);
+            select = scanner.nextInt();
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
                 System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
+                String newName = scanner.next();
+                Item newItem = new Item(newName);
+                tracker.add(newItem);
             } else if (select == 1) {
                 System.out.println("=== Showing all items ====");
                 Item[] allItems = tracker.findAll();
                 if (allItems.length == 0) {
                     System.out.println("Заявок нет");
                 } else {
-                    for (Item item : allItems) {
-                        System.out.println(item);
+                    for (Item oneItem : allItems) {
+                        System.out.println(oneItem);
                     }
                 }
             } else if (select == 2) {
                 System.out.println("=== Editing item ====");
                 System.out.println("Введите ID заявки, которую необходимо изменить: ");
-                int idForDelete = scanner.nextInt();
-                System.out.println("Введите ID заявки, на которую необходимо заменить: ");
-                int idForRemove = scanner.nextInt();
-                if (tracker.findById(idForDelete) == null | tracker.findById(idForRemove) == null) {
-                    System.out.println("Ошибка при перемещении заявки");
-                } else {
-                    Item itemForRemove = new Item(tracker.findById(idForRemove).getName());
-                    boolean isRemoved = tracker.replace(idForDelete, itemForRemove);
-                    System.out.println(isRemoved ? "Заявка успешно перемещена" : "Ошибка при перемещении заявки");
-                }
+                int idForEdit = scanner.nextInt();
+                System.out.println("Введите имя новой заявки: ");
+                String nameForEdit = scanner.next();
+                Item itemForEdit = new Item();
+                itemForEdit.setName(nameForEdit);
+                boolean isReplaced = tracker.replace(idForEdit, itemForEdit);
+                System.out.println(isReplaced ? "Заявка успешно изменена" : "Ошибка при изменении заявки");
             } else if (select == 3) {
                 System.out.println("=== Deleting item ====");
                 System.out.println("Введите ID заявки, которую необходимо удалить: ");
@@ -53,18 +46,18 @@ public class StartUI {
                 System.out.println("=== Finding item by ID ====");
                 System.out.println("Введите ID заявки, которую необходимо найти: ");
                 int idForSearch = scanner.nextInt();
-                Item found = tracker.findById(idForSearch);
-                System.out.println(found == null ? "Заявка с таким id не найдена" : found);
+                Item itemFound = tracker.findById(idForSearch);
+                System.out.println(itemFound == null ? "Заявка с таким id не найдена" : itemFound);
             } else if (select == 5) {
                 System.out.println("=== Finding item by name ====");
                 System.out.println("Введите имя заявки, которую необходимо найти: ");
-                String nameForSearch = scanner.nextLine();
+                String nameForSearch = scanner.next();
                 Item[] foundItems = tracker.findByName(nameForSearch);
                 if (foundItems.length == 0) {
                     System.out.println("Заявки с таким именем не найдены");
                 } else {
-                    for (Item item : foundItems) {
-                        System.out.println(item);
+                    for (Item oneItem : foundItems) {
+                        System.out.println(oneItem);
                     }
                 }
             } else if (select == 6) {
