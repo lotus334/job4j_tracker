e5a3e18447ca4c8c96e55f329b3ace6cbf2a03dc
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-21 08:18:38 +0700

    1.1. Тесты на StartUI. [#386374]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,6 +10,28 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2", "0"}
+                );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit Program ====%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. === Exit Program ====%n"
+                )
+        ));
+    }
+
+    @Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
