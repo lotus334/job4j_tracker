120209251896f1de698b190c331933e3c17289ec
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-21 12:43:52 +0700

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#386366] часть 2. Добавил тест класса ValidateInput для корректных данных и на проверку выходящих данных.

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -7,13 +7,41 @@ import static org.junit.Assert.assertThat;
 public class ValidateInputTest {
 
     @Test
-    public void askStr() {
+    public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu");
+        int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"11"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(11));
+
+    }
+
+    @Test
+    public void whenInvalidInputCheckOutput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"eleven", "11"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(out.toString(), is(
+                String.format(
+                        "Please enter validate data again.%n"
+                )
+        ));
+
     }
 }
