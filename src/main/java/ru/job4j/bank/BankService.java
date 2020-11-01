586609dccfed6bec3fdb9c1b5e684f5525c43d8f
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-01 18:19:19 +0700

    3. Банковские переводы [#386338]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,67 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        if (accounts.contains(account)) {
+            System.out.println("The account was added earlier");
+        } else {
+            accounts.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        Set<User> setOfUsers = users.keySet();
+        for (User user : setOfUsers) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            int index = accounts.indexOf(new Account(requisite, -1));
+            if (index != -1) {
+                return accounts.get(index);
+            }
+            /*
+            for (Account acc : accounts) {
+                if (acc.getRequisite().equals(requisite)) {
+                    return acc;
+                }
+            }
+             */
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount == null  || destAccount == null) {
+            System.out.println("Cant find account");
+        } else if (srcAccount.getBalance() < amount) {
+            System.out.println("Insufficient funds");
+        } else {
+            rsl = true;
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+        }
+        return rsl;
+    }
+}
