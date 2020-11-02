153795f5459924fa40575ab1ea885d46a41cd2a6
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-02 20:38:22 +0700

    0. Сортировка [#386326]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 
-public class Item {
+public class Item implements Comparable<Item> {
     private int id;
     private String name;
     LocalDateTime created = LocalDateTime.now();
@@ -48,5 +48,10 @@ public class Item {
                 ", name='" + name + '\'' +
                 ", created=" + created +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Item another) {
+        return Integer.compare(getId(), another.getId());
     }
 }
---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,108 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ItemTest {
+
+    @Test
+    public void compareToAscendingOrderById() {
+        Item item1 = new Item(1, "first");
+        Item item3 = new Item(3, "third");
+        Item item2 = new Item(2, "second");
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list);
+        List<Item> expected = Arrays.asList(item1, item2, item3);
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void compareToReverseOrderById() {
+        Item item1 = new Item(1, "first");
+        Item item3 = new Item(3, "third");
+        Item item2 = new Item(2, "second");
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list, Collections.reverseOrder());
+        List<Item> expected = Arrays.asList(item3, item2, item1);
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void compareToAscendingOrderByName() {
+        Item item1 = new Item(3, "first");
+        Item item3 = new Item(1, "third");
+        Item item2 = new Item(2, "second");
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list, new SortByNameItem());
+        List<Item> expected = Arrays.asList(item1, item2, item3);
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void compareToReverseOrderByName() {
+        Item item1 = new Item(3, "first");
+        Item item3 = new Item(1, "third");
+        Item item2 = new Item(2, "second");
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list, Collections.reverseOrder(new SortByNameItem()));
+        List<Item> expected = Arrays.asList(item3, item2, item1);
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void compareToAscendingOrderByDate() {
+        Item item1 = new Item(1, "first");
+        Item item3 = new Item(3, "third");
+        Item item2 = new Item(2, "second");
+        item1.created = LocalDateTime.of(2000, 11, 11, 11, 11);
+        item3.created = LocalDateTime.of(2001, 11, 11, 11, 11);
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list, new SortByDateItem());
+        List<Item> expected = Arrays.asList(item1, item3, item2);
+        assertThat(list, is(expected));
+    }
+
+    @Test
+    public void compareToReverseOrderByDate() {
+        Item item1 = new Item(1, "first");
+        Item item3 = new Item(3, "third");
+        Item item2 = new Item(2, "second");
+        item1.created = LocalDateTime.of(2000, 11, 11, 11, 11);
+        item3.created = LocalDateTime.of(2001, 11, 11, 11, 11);
+        List<Item> list = Arrays.asList(
+                item1,
+                item3,
+                item2
+        );
+        Collections.sort(list, new SortByDateItem().reversed());
+        List<Item> expected = Arrays.asList(item2, item3, item1);
+        assertThat(list, is(expected));
+    }
+}
