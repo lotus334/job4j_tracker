e8169dfdde5f30c08f79ae133c4abecd27467ce8
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-02 21:20:34 +0700

    1. Организовать сортировку User [#386324] В компараторе SortByNameUser убрал сравнение через символы и сделал через строку.

--- src/main/java/ru/job4j/collection/SortByNameUser.java
+++ src/main/java/ru/job4j/collection/SortByNameUser.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class SortByNameUser implements Comparator<User> {
     @Override
     public int compare(User o1, User o2) {
-        return CharSequence.compare(o1.getName(), o2.getName());
+        return o1.getName().compareTo(o2.getName());
     }
 }
