86e533c4d6c55065a6f2e5046f66ae82d6111e3f
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-18 19:56:57 +0700

    1. Метод .of [#386172]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -2,40 +2,35 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        Set<Account> expect = Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
-                )
         );
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
     @Test
     public void doNotSent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("123", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        Set<Account> expect = Set.of(
                         new Account("123", "Petr Arsentev", "000001")
-                )
         );
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
