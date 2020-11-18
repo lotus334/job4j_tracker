82eb9253b85b2a120bdfba1588fd10be45feecaf
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-18 18:09:40 +0700

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#386159]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,6 +1,9 @@
 package ru.job4j.bank;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
@@ -10,44 +13,30 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
-            if (accounts.contains(account)) {
-                System.out.println("The account was added earlier");
-            } else {
-                accounts.add(account);
-            }
-        }
+        users.computeIfPresent(findByPassport(passport),
+                ((user, accounts) -> {
+                    if (accounts.contains(account)) {
+                        System.out.println("The account was added earlier");
+                    } else {
+                        accounts.add(account);
+                    }
+                    return accounts;
+                })
+        );
     }
 
     public User findByPassport(String passport) {
-        Set<User> setOfUsers = users.keySet();
-        for (User user : setOfUsers) {
-            if (user.getPassport().equals(passport)) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet().stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
-            int index = accounts.indexOf(new Account(requisite, -1));
-            if (index != -1) {
-                return accounts.get(index);
-            }
-            /*
-            for (Account acc : accounts) {
-                if (acc.getRequisite().equals(requisite)) {
-                    return acc;
-                }
-            }
-             */
-        }
-        return null;
+        return users.getOrDefault(findByPassport(passport), new ArrayList<>()).stream()
+                .filter(account -> account.getRequisite().equals(requisite))
+                .findFirst()
+                .orElse(null);
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
